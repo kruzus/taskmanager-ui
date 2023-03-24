b97a926a7fec62b23ccad75304b285483eb8f509
Bek <kruzus@live.com>
2023-03-24 18:33:34 -0400

    lol

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,20 @@ export default function Home() {
         <title>Task Manager - Manage Your Tasks</title>
       </Head>
       <main >
-        <h1>Task Manager UI</h1>
+      <div>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Organize Your Tasks with Ease
+            </h2>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 mx-auto">
+              Manage your tasks and stay productive with our easy-to-use task manager application.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
       </main>
     </>
   )
