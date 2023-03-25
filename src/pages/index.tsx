b821cdf1dd9d1f4786cc1cacae18a317bba44216
Bek <kruzus@live.com>
2023-03-25 02:53:51 -0400

    stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,6 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-     <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Task Manager - The perfect tool to manage your tasks and improve your productivity. Create, organize and complete your tasks easily." />
-        <meta name="keywords" content="task manager, productivity, create tasks, complete tasks, organize tasks" />
-        <meta name="author" content="kruzus" />
-        <title>Task Manager - Manage Your Tasks</title>
-      </Head>
       <main >
         <h1>Task Manager UI</h1>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
+  theme:{
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+  },
   daisyui: {
     styled: true,
     themes: false,
